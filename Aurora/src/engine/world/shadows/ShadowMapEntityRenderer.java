f4b896bb4a5713f55a6de7b141a6b8417680adcb
Greg <gmfoss645@gmail.com>
2018-01-06 17:02:14 -0500

    Fixing Multi-threading

--- Aurora/src/engine/world/shadows/ShadowMapEntityRenderer.java
+++ Aurora/src/engine/world/shadows/ShadowMapEntityRenderer.java
@@ -22,8 +22,7 @@ public class ShadowMapEntityRenderer {
 
 	/**
 	 * @param shader
-	 *            - the simple shader program being used for the shadow render
-	 *            pass.
+	 *            - the simple shader program being used for the shadow render pass.
 	 * @param projectionViewMatrix
 	 *            - the orthographic projection matrix multiplied by the light's
 	 *            "view" matrix.
@@ -34,33 +33,34 @@ public class ShadowMapEntityRenderer {
 	}
 
 	/**
-	 * Renders entieis to the shadow map. Each model is first bound and then all
-	 * of the entities using that model are rendered to the shadow map.
+	 * Renders entieis to the shadow map. Each model is first bound and then all of
+	 * the entities using that model are rendered to the shadow map.
 	 * 
 	 * @param entities
 	 *            - the entities to be rendered to the shadow map.
 	 */
 	protected void render(Map<TexturedModel, List<Entity>> entities) {
-		for (TexturedModel model : entities.keySet()) {
-			RawModel rawModel = model.getRawModel();
-			bindModel(rawModel);
-			GL13.glActiveTexture(GL13.GL_TEXTURE0);
-			GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getID());
-			if(model.getTexture().hasTransparency()){
-				MasterRenderer.disableCulling();
+		synchronized (entities) {
+			for (TexturedModel model : entities.keySet()) {
+				RawModel rawModel = model.getRawModel();
+				bindModel(rawModel);
+				GL13.glActiveTexture(GL13.GL_TEXTURE0);
+				GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getID());
+				if (model.getTexture().hasTransparency()) {
+					MasterRenderer.disableCulling();
+				}
+				for (Entity entity : entities.get(model)) {
+					prepareInstance(entity);
+					GL11.glDrawElements(GL11.GL_TRIANGLES, rawModel.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+				}
+				if (model.getTexture().hasTransparency()) {
+					MasterRenderer.enableCulling();
+				}
 			}
-			for (Entity entity : entities.get(model)) {
-				prepareInstance(entity);
-				GL11.glDrawElements(GL11.GL_TRIANGLES, rawModel.getVertexCount(),
-						GL11.GL_UNSIGNED_INT, 0);
-			}
-			if(model.getTexture().hasTransparency()){
-				MasterRenderer.enableCulling();
-			}
+			GL20.glDisableVertexAttribArray(0);
+			GL20.glDisableVertexAttribArray(1);
+			GL30.glBindVertexArray(0);
 		}
-		GL20.glDisableVertexAttribArray(0);
-		GL20.glDisableVertexAttribArray(1);
-		GL30.glBindVertexArray(0);
 	}
 
 	/**
@@ -78,16 +78,17 @@ public class ShadowMapEntityRenderer {
 	}
 
 	/**
-	 * Prepares an entity to be rendered. The model matrix is created in the
-	 * usual way and then multiplied with the projection and view matrix (often
-	 * in the past we've done this in the vertex shader) to create the
-	 * mvp-matrix. This is then loaded to the vertex shader as a uniform.
+	 * Prepares an entity to be rendered. The model matrix is created in the usual
+	 * way and then multiplied with the projection and view matrix (often in the
+	 * past we've done this in the vertex shader) to create the mvp-matrix. This is
+	 * then loaded to the vertex shader as a uniform.
 	 * 
 	 * @param entity
 	 *            - the entity to be prepared for rendering.
 	 */
 	private void prepareInstance(Entity entity) {
-		Matrix4f modelMatrix = entity.getTransformationMatrix(); //Calculator.createTransformationMatrix(entity.getPosition(),entity.getRotation(), entity.getScale());
+		Matrix4f modelMatrix = entity.getTransformationMatrix(); // Calculator.createTransformationMatrix(entity.getPosition(),entity.getRotation(),
+																	// entity.getScale());
 		Matrix4f mvpMatrix = Matrix4f.mul(projectionViewMatrix, modelMatrix, null);
 		shader.loadMvpMatrix(mvpMatrix);
 	}
